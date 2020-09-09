2de924eb13cbfb36d6ab28d4ef1a064b17e5cf2c
sagartmg <tamangsagar70@gmail.com>
2020-09-09 15:23:00 +0545

    d

--- src/CrochetsNepal/contentful.js
+++ src/CrochetsNepal/contentful.js
@@ -16,6 +16,6 @@ import {createClient} from "contentful"
 
 export default createClient({
   space: process.env.REACT_APP_API_SPACE,
-  accessToken:process.env.REACT_APP_API_TOKEN
+  accessToken:process.env.REACT_APP_ACCESS_TOKEN
 
 });
